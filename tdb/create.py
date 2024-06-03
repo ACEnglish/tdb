766356d5ec5a096f3242066c8bcc39cb7c4e0168
Adam English <ACEnglish@gmail.com>
2024-06-03 13:23:12 -0500

    optional create memory usage limit

--- tdb/create.py
+++ tdb/create.py
@@ -4,6 +4,7 @@ Faster creation of a tdb
 parse each line and write the outputs.
 No need to worry about getting crazy with the indexes or joins
 """
+import gc
 import os
 import sys
 import logging
@@ -15,6 +16,8 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+
+#pylint: disable=global-statement
 
 DTYPES = {"LocusID": (pa.uint32(), np.uint32),
           "chrom": (pa.string(), str),
@@ -32,6 +35,9 @@ L_COLUMNS = ["LocusID", "chrom", "start", "end"]
 A_COLUMNS = ["LocusID", "allele_number", "allele_length", "sequence"]
 S_COLUMNS = ["LocusID", "allele_number", "spanning_reads", "length_range_lower",
              "length_range_upper", "average_methylation"]
+# Assume 100MB is always used
+USEDMEM = 1e6
+AVAILMEM = sys.maxsize
 
 def check_args(args):
     """
@@ -40,7 +46,7 @@ def check_args(args):
     check_fail = False
 
     if os.path.exists(args.output):
-        logging.error(f"Output  {args.output} already exists")
+        logging.error(f"Output {args.output} already exists")
         check_fail = True
     if not args.output.endswith(".tdb"):
         logging.error(f"Output {args.output} must end with `.tdb`")
@@ -119,26 +125,31 @@ def translate_entry(entry, locus_id):
     a list of allele rows
     a dictionary of sample: list of sample rows
     """
+    global USEDMEM
     locus = [locus_id, entry.chrom, entry.start, entry.stop]
+    USEDMEM += sys.getsizeof(locus)
     alleles = [(locus_id, allele_number, len(sequence), sequence.encode("utf8"))
                for allele_number, sequence in enumerate(entry.alleles)]
+    USEDMEM += sys.getsizeof(alleles)
     samples = {}
     for sample, m_d in entry.samples.items():
         samples[sample] = sample_extract(locus_id, m_d)
-
+    # Approximate usage of each row of a sample table
+    USEDMEM += 400 * len(samples)
     return locus, alleles, samples
 
-def convert_buffer(vcf, samples, stats, num_lines):
+def convert_buffer(vcf, samples, stats):
     """
-    Converts upto num_lines number of vcf entries
+    Converts a number of vcf entries.
+    Tries to monitor memory to not buffer too many
     """
-    buffer = {'locus':[],
+    m_buffer = {'locus':[],
               'allele':[],
               'sample': {_:[] for _ in samples}
               }
     # Flag for telling main loop when we're finished
     cvt_any = False
-    while num_lines > 0:
+    while AVAILMEM > USEDMEM:
         try:
             entry = next(vcf)
         except StopIteration:
@@ -147,67 +158,75 @@ def convert_buffer(vcf, samples, stats, num_lines):
         cvt_any = True
         cur_locus, cur_allele, cur_sample = translate_entry(entry, stats['locus'])
 
-        buffer['locus'].append(cur_locus)
-        buffer['allele'].extend(cur_allele)
+        m_buffer['locus'].append(cur_locus)
+        m_buffer['allele'].extend(cur_allele)
         num_samples = 0
         for name, rows in cur_sample.items():
             num_samples += len(rows)
-            buffer['sample'][name].extend(rows)
+            m_buffer['sample'][name].extend(rows)
 
         stats['locus'] += 1
         stats['allele'] += len(cur_allele)
         stats['sample'] += num_samples
 
-        num_lines -= 1
-    return buffer, cvt_any
+    return m_buffer, cvt_any
 
 def write_tables(cur_tables, tables):
     """
     Write the cur_tables entries to the output tables
     So since cur_tables will have a list of vales, I should probably do pa.Table from pandas
     """
+    global USEDMEM
     schema = pa.schema({key: DTYPES[key][0] for key in L_COLUMNS})
-    ldf = pd.DataFrame(cur_tables["locus"], columns=L_COLUMNS)
+    ldf = pd.DataFrame(cur_tables["locus"], columns=L_COLUMNS, copy=False)
     locus = pa.Table.from_pandas(ldf, schema=schema, preserve_index=False)
     tables['locus'].write(locus)
+    tables['locus'].flush()
 
     schema = pa.schema({key: DTYPES[key][0] for key in A_COLUMNS})
-    adf = pd.DataFrame(cur_tables["allele"], columns=A_COLUMNS)
+    adf = pd.DataFrame(cur_tables["allele"], columns=A_COLUMNS, copy=False)
     allele = pa.Table.from_pandas(adf, schema=schema, preserve_index=False)
     tables['allele'].write(allele)
+    tables['allele'].flush()
 
     schema = pa.schema({key: DTYPES[key][0] for key in S_COLUMNS})
     for name, out_samp in tables["sample"].items():
-        sdf = pd.DataFrame(cur_tables["sample"][name], columns=S_COLUMNS)
+        sdf = pd.DataFrame(cur_tables["sample"][name], columns=S_COLUMNS, copy=False)
         sample = pa.Table.from_pandas(sdf, schema=schema, preserve_index=False)
         out_samp.write(sample)
+        out_samp.flush()
+    # Reset memory
+    USEDMEM = 1e6
 
 def create_main(args):
     """
     Create a new tdb from multiple input calls
     """
+    global AVAILMEM
     parser = argparse.ArgumentParser(prog="tdb create", description=__doc__,
                             formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-o", "--output", metavar="OUT", required=True,
                         help="Output tdb directory")
-    parser.add_argument("-b", "--buffer", metavar="BUF", type=int,
-                        help="Number of vcf lines to buffer before write (off)")
+    parser.add_argument("--mem", metavar="MEM", type=int,
+                        help="Memory in GB available to buffer reading (unlimited)")
     parser.add_argument("--no-compress", action="store_false",
                         help="Don't compress the database")
+    parser.add_argument("--debug", action="store_true",
+                        help="Verbose logging")
     parser.add_argument("input", metavar="IN",
                         help="VCF file")
     args = parser.parse_args(args)
 
-    truvari.setup_logging()
+    truvari.setup_logging(args.debug)
 
     if check_args(args):
         logging.error("Cannot create database. Exiting")
         sys.exit(1)
 
-    if args.buffer is None:
-        args.buffer = sys.maxsize
-
     truvari.setup_logging()
+    if args.mem is not None:
+        AVAILMEM = args.mem * 1e9
+
     os.mkdir(args.output)
 
     vcf = pysam.VariantFile(args.input)
@@ -217,10 +236,13 @@ def create_main(args):
     tables = make_parquets(samples, args.output, args.no_compress)
     logging.info("Converting VCF with %d samples", len(samples))
     while True:
-        cur_tables, cvt_any = convert_buffer(vcf, samples, stats, args.buffer)
+        cur_tables, cvt_any = convert_buffer(vcf, samples, stats)
         if not cvt_any:
             break
+        logging.debug("writing. totals %s", stats)
         write_tables(cur_tables, tables)
+        del cur_tables
+        gc.collect()
 
     tables['locus'].close()
     tables['allele'].close()
